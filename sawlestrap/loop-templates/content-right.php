f9600146f0484298d450eda2a73c3c48ee580361
Tanngrisnr <Fredrik.Wintzell@gmail.com>
2020-02-15 14:43:46 +0100

    Add files via upload

---
+++ sawlestrap/loop-templates/content-right.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Post rendering content according to caller of get_template_part.
+ *
+ * @package swalestrap
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+?>
+<div class="row justify-content-end">
+	<div class="col-sm-6">
+<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+	<header class="entry-header">
+
+		<?php
+		the_title(
+			sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
+			'</a></h2>'
+		);
+		?>
+
+
+
+	</header><!-- .entry-header -->
+
+	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+
+	<div class="entry-content">
+
+		<?php the_excerpt(); ?>
+
+		<?php
+		wp_link_pages(
+			array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'Swale' ),
+				'after'  => '</div>',
+			)
+		);
+		?>
+
+	</div><!-- .entry-content -->
+
+
+
+</article><!-- #post-## -->
+</div>
+</div>
